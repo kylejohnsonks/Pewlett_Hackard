348e9540dccced7a4d5b83089769d1afd8e91298
kylejohnsonks <92126368+kylejohnsonks@users.noreply.github.com>
2021-12-11 15:30:37 -0600

    a

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -5,28 +5,32 @@ SELECT e.emp_no,
 	t.title,
 	t.from_date,
 	t.to_date
-INTO all_titles
+INTO retirement_titles
 FROM employees as e
 JOIN titles as t
 ON (e.emp_no=t.emp_no)
 WHERE (e.birth_date Between '1952-01-01' and '1955-12-31')
 ORDER BY e.emp_no, t.to_date DESC;
-
-
 
 -- Use Dictinct with Orderby to remove duplicate rows, create unique titles table
-SELECT DISTINCT ON (e.emp_no) e.emp_no,
-	e.first_name,
-	e.last_name,
-	t.title,
-	t.from_date,
-	t.to_date
-INTO unique_titles
-FROM employees as e
-JOIN titles as t
-ON (e.emp_no=t.emp_no)
+SELECT DISTINCT ON (r.emp_no) r.emp_no,
+	r.first_name,
+	r.last_name,
+	r.title
+INTO unique_titless
+FROM retirement_titles r
+ORDER BY r.to_date DESC;
+
+--retrieve count of employees by most recent job title who are about to retire
+SELECT COUNT(u.title), u.title
+INTO retiring_titles
+FROM unique_titles u
+JOIN employees e
+ON e.emp_no=u.emp_no
 WHERE (e.birth_date Between '1952-01-01' and '1955-12-31')
-ORDER BY e.emp_no, t.to_date DESC;
+GROUP BY u.title
+ORDER BY COUNT(u.title) DESC;
+
 
 --reate a mentorship-eligibility table 
 --that holds the current employees who were born between January 1, 1965 and December 31, 1965.
@@ -40,8 +44,11 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
 INTO mentorship_eligible
 FROM employees e
 JOIN dept_emp de
-ON (e.emp_no=de.emp_no)
+ON e.emp_no=de.emp_no
 JOIN titles t
-ON (e.emp_no=t.emp_no)
+ON e.emp_no=t.emp_no
 WHERE (e.birth_date Between '1965-01-01' and '1965-12-31') and (de.to_date = '9999-01-01')
 ORDER BY e.emp_no, t.to_date DESC;
+
+
+
